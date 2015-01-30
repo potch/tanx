809cf7c919ed6402f13b9535ba7df9a98f635fe2
Matt Claypotch <thepotch@gmail.com>
2015-01-30 14:50:49 -0800

    joystick support

--- client.js
+++ client.js
@@ -11,34 +11,34 @@ pc.script.create('client', function (context) {
         initialize: function () {
             this.tanks = context.root.getChildren()[0].script.tanks;
             this.bullets = context.root.getChildren()[0].script.bullets;
-            
+
             var self = this;
             var socket = this.socket = new m.net.ws({ url: 'wss://tanx.playcanvas.com/' });
 
             socket.on('init', function(data) {
                 self.id = data.id;
             });
-            
+
             socket.on('tank.new', function(data) {
                 self.tanks.new(data);
             });
-            
+
             socket.on('tank.delete', function(data) {
                 self.tanks.delete(data);
             });
-            
+
             socket.on('update', function(data) {
                 self.tanks.updateData(data.tanks);
-                
+
                 var i = 0;
-                
+
                 if (data.tanksRespawn) {
                     i = data.tanksRespawn.length;
                     while(i--) {
                         self.tanks.respawn(data.tanksRespawn[i]);
                     }
                 }
-                
+
                 if (data.bulletsDelete) {
                     i = data.bulletsDelete.length;
                     while(i--) {
@@ -46,14 +46,14 @@ pc.script.create('client', function (context) {
                     }
                 }
             });
-            
+
             socket.on('bullet.new', function(data) {
                 self.bullets.new(data);
             });
-            
+
             context.mouse.on('mousedown', this.onMouseDown, this);
             context.mouse.on('mouseup', this.onMouseUp, this);
-            
+
             this.mouseState = false;
         },
 
@@ -63,31 +63,50 @@ pc.script.create('client', function (context) {
                 context.keyboard.isPressed(pc.input.KEY_D) - context.keyboard.isPressed(pc.input.KEY_A),
                 context.keyboard.isPressed(pc.input.KEY_S) - context.keyboard.isPressed(pc.input.KEY_W)
             ];
-            
+
+            // sum in gamepad axis
+            movement[0] += context.gamepads.getAxis(pc.PAD_1, pc.PAD_L_STICK_X);
+            movement[1] += context.gamepads.getAxis(pc.PAD_1, pc.PAD_L_STICK_Y);
+
+
+            // determine firing state
+            var gpx = context.gamepads.getAxis(pc.PAD_1, pc.PAD_R_STICK_X);
+            var gpy = context.gamepads.getAxis(pc.PAD_1, pc.PAD_R_STICK_Y);
+
+            if (gpx * gpx + gpy * gpy > .25 && this) {
+              this.shoot(true);
+              this.gpShot = true;
+            } else {
+              if (this.gpShot) {
+                this.shoot(false);
+                this.gpShot = false;
+              }
+            }
+
             // rotate vector
             var t =       movement[0] * Math.sin(Math.PI * 0.75) - movement[1] * Math.cos(Math.PI * 0.75);
             movement[1] = movement[1] * Math.sin(Math.PI * 0.75) + movement[0] * Math.cos(Math.PI * 0.75);
             movement[0] = t;
-            
+
             // check if it is changed
             if (movement[0] !== this.movement[0] || movement[1] != this.movement[1]) {
                 this.movement = movement;
                 this.socket.send('move', this.movement);
             }
         },
-        
+
         onMouseDown: function() {
             this.shoot(true);
         },
-        
+
         onMouseUp: function() {
             this.shoot(false);
         },
-        
+
         shoot: function(state) {
             if (this.shootingState !== state) {
                 this.shootingState = state;
-                
+
                 this.socket.send('shoot', this.shootingState);
             }
         }
--- link.js
+++ link.js
@@ -20,12 +20,21 @@ pc.script.create('link', function (context) {
 
         update: function (dt) {
             if (this.link) {
+
+                var gpx = context.gamepads.getAxis(pc.PAD_1, pc.PAD_R_STICK_X);
+                var gpy = context.gamepads.getAxis(pc.PAD_1, pc.PAD_R_STICK_Y);
+
+                if (gpx * gpx + gpy * gpy > .25) {
+                  this.angle = Math.floor(Math.atan2(gpx, gpy) / (Math.PI / 180) + 45);
+                  this.link.targeting(this.angle);
+                }
+
                 // rotate vector
                 var rot = [ this.mPos.x, this.mPos.y ];
                 var t =  rot[0] * Math.sin(Math.PI * 0.75) - rot[1] * Math.cos(Math.PI * 0.75);
                 rot[1] = rot[1] * Math.sin(Math.PI * 0.75) + rot[0] * Math.cos(Math.PI * 0.75);
                 rot[0] = t;
-                
+
                 tmpVec.set(
                     this.link.getPosition().x + 9 + (rot[0] / (context.graphicsDevice.width / 2) * 4),
                     14,
@@ -33,30 +42,30 @@ pc.script.create('link', function (context) {
                 )
                 this.entity.setPosition(tmpVec.lerp(this.entity.getPosition(), tmpVec, 0.1));
             }
-            
+
             if (Date.now() - this.lastSend > 100 && this.angle !== this.lastAngle) {
                 this.lastSend = Date.now();
                 this.lastAngle = this.angle;
-                
+
                 this.client.socket.send('target', this.angle);
             }
         },
-        
+
         onMouseMove: function(evt) {
             if (this.link) {
                 this.mPos.set(evt.x - (context.graphicsDevice.width / 2), evt.y - (context.graphicsDevice.height / 2));
                 this.vec.copy(this.mPos).normalize();
-                
+
                 // rotate vector
                 var t =      this.vec.x * Math.sin(Math.PI * 0.75) - this.vec.y * Math.cos(Math.PI * 0.75);
                 this.vec.y = this.vec.y * Math.sin(Math.PI * 0.75) + this.vec.x * Math.cos(Math.PI * 0.75);
                 this.vec.x = t;
-                
+
                 this.angle = Math.floor(Math.atan2(this.vec.x, this.vec.y) / (Math.PI / 180));
                 this.link.targeting(this.angle);
             }
         },
-        
+
         link: function(tank) {
             this.link = tank;
         }
